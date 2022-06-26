d716d2cae7f500aae93432c193f902823bf7e655
RichardDoDeal <mingazov.rishat1998@gmail.com>
2022-06-26 18:10:32 +0300

    add github storage to flow

--- simple_flow/docker_flow.py
+++ simple_flow/docker_flow.py
@@ -2,7 +2,11 @@ from cProfile import label
 import prefect
 from prefect import task, Flow
 from prefect.run_configs import DockerRun
-from prefect.storage import Local
+from prefect.storage import GitHub
+
+IMAGE='prefecthq/prefect:latest-python3.8'
+REPO='RichardDoDeal/prefect_datalearn'
+PATH='simple_flow/docker_flow.py'
 
 @task
 def hello_task():
@@ -11,7 +15,9 @@ def hello_task():
 
 
 with Flow("hello-docker-flow",
-		run_config=DockerRun(image='prefecthq/prefect:latest-python3.8', labels=['docker'])) as flow:   
+		storage=GitHub(repo=REPO, path=PATH),
+		run_config=DockerRun(image=IMAGE,
+		labels=['docker'])) as flow:   
     hello_task()
 
 flow.register(project_name="da_proj", labels=['docker'])
